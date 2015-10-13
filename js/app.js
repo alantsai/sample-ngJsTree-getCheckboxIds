9c6411a635b7d2b6a7a01ec6bf7ca85fe5fe217f
Alan Tsai <alan@alantsai.net>
2015-10-13 23:34:54 +0800

    simple jsTree data

--- js/app.js
+++ js/app.js
@@ -1,5 +1,18 @@
-var app = angular.module("app", []);
+var app = angular.module("app", ["ngJsTree"]);
 
-app.controller("jsTreeController", function($scope){
-	
+app.controller("jsTreeController", function ($scope) {
+
+	$scope.init = function () {
+		$scope.model = $scope.model || {};
+		$scope.model.jsTreeData = [
+			{ "id": "ajson1", "parent": "#", "text": "Simple root node" },
+			{ "id": "ajson2", "parent": "#", "text": "Root node 2" },
+			{ "id": "ajson3", "parent": "ajson2", "text": "Child 1" },
+			{ "id": "ajson4", "parent": "ajson2", "text": "Child 2" },
+		];
+		
+		$scope.treeConfig = {};
+	}
+
+	$scope.init();
 })
